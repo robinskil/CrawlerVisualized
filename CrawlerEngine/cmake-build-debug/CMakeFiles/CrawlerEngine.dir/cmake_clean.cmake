99c0befc7c1c9270745cddbd0be4bdf7c65f542e
Robin <robin.kooyman.work@gmail.com>
2021-02-12 21:05:10 +0100

    Setup

---
+++ CrawlerEngine/cmake-build-debug/CMakeFiles/CrawlerEngine.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CrawlerEngine.dir/main.c.o"
+  "CrawlerEngine"
+  "CrawlerEngine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CrawlerEngine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CrawlerEngine/cmake-build-debug/CMakeFiles/CrawlerEngine.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for CrawlerEngine.
+# This may be replaced when dependencies are built.
